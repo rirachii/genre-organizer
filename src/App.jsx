9293c5c84a2a30740b248c94e9bcf5b57fc3c039
Michelle Weng <82119639+rirachii@users.noreply.github.com>
2025-01-14 15:33:23 -0500

    Add React application base files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GenreOrganizer from './components/GenreOrganizer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <GenreOrganizer />
+    </div>
+  );
+}
+
+export default App;
